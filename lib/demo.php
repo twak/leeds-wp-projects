ace609bbf1de6f3ca0d148644bf9da3b63ca4124
twakelly@gmail.com <twakelly@gmail.com>
2019-05-30 11:27:26 +0100

    working demo :)

--- lib/demo.php
+++ lib/demo.php
@@ -53,8 +53,12 @@ function twak_register_routes() {
 
 add_action('parse_request', 'demo');
 
-function demo() {
-    if($_SERVER["REQUEST_URI"] == '/demo') {
+function demo($params) {
+
+
+
+    if( strtok ( $_SERVER["REQUEST_URI"], '?' ) == '/demo')
+    {
 
         ?>
         <!DOCTYPE html>
@@ -64,6 +68,8 @@ function demo() {
             <title>VCG demo</title>
         </head>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<!--        <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>-->
+<!--        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css">-->
 
         <style>
 
@@ -73,8 +79,20 @@ function demo() {
             }
 
         </style>
-    <?php
-        demo_auto_grid(1, "window", 100);
+
+
+
+        <?php
+
+        $speed = 300;
+        if (array_key_exists('speed', $_GET))
+            $speed = (int) $_GET["speed" ];
+
+        $page = 4000;
+        if (array_key_exists('page', $_GET))
+            $page = (int) $_GET["page" ];
+
+        demo_auto_grid(1, 256, "window", $speed, $page );
     ?>
         <body>
 
@@ -96,7 +114,7 @@ function demo() {
     }
 }
 
-function demo_auto_grid($add, $element, $speed) {
+function demo_auto_grid($add /* 0 if shortcode, 1 if fullscreen */, $px_size, $element, $speed, $page) {
     ?>
 
         <script>
@@ -105,16 +123,16 @@ function demo_auto_grid($add, $element, $speed) {
                 console.log($(".auto-grid")[0]);
                 $(".auto-grid").empty();
 
-                var rows =  Math.floor( $( <?php echo ($element); ?> ).height() / 256 ) +  <?php echo ($add); ?>; //here's your number of rows and columns
-                var cols =  Math.floor( $( <?php echo ($element); ?> ).width() / 256 )+  <?php echo ($add); ?>;
+                var rows =  Math.floor( $( <?php echo ($element); ?> ).height() / <?php echo($px_size)?> ) +  <?php echo ($add); ?>; //here's your number of rows and columns
+                var cols =  Math.floor( $( <?php echo ($element); ?> ).width() / <?php echo($px_size)?> )+  <?php echo ($add); ?>;
                 var table = $('<table cellspacing="0" cellpadding="0"><tbody>');
                 window.coords = [];
                 for(var r = 0; r < rows; r++)
                 {
-                    var tr = $("<tr style='height:256px'>");
+                    var tr = $("<tr style='height:<?php echo($px_size)?>px'>");
                     for (var c = 0; c < cols; c++) {
                         var id = 'x_' + c + '_y_' + r ;
-                        $('<td class="cell" id="' + id + '" ><a target="<?php echo ($add == 1 ? "iframe_a" : "_self"); ?>" href="/"><img src="<?php get_site_url() ?>/wp-content/plugins/leeds-wp-projects/resources/logo.svg"/></a></td>').appendTo(tr);
+                        $('<td class="cell" id="' + id + '" ><a class="the_link" target="<?php echo ($add == 1 ? "iframe_a" : "_self"); ?>" href="/"><img src="<?php get_site_url() ?>/wp-content/plugins/leeds-wp-projects/resources/logo_blue.svg"/></a></td>').appendTo(tr);
                         window.coords.push(id);
                     }
                     tr.appendTo(table);
@@ -132,6 +150,7 @@ function demo_auto_grid($add, $element, $speed) {
             });
 
             $( document ).ready(function() {
+                window.data_cache=[];
                 fetchData();
                 build();
 
@@ -143,6 +162,15 @@ function demo_auto_grid($add, $element, $speed) {
                 $(this).keypress(function (e) {
                     window.idleTime = 0;
                 });
+
+                window.idle_limit_sec = 60; // if the mouse isn't moved for this long, update webpage
+                window.update_limit_ms = <?php echo($page); ?>; // update the shared this often, upate webpage
+
+                $(".the_link").attr('alt', "VCG" );
+                $(".the_link").attr('title', "VCG" );
+
+                window.setInterval(fetchData, <?php echo($speed* 10); ?> )
+                window.setInterval(displayData, <?php echo($speed); ?>);
             });
 
             function timerIncrement() {
@@ -150,23 +178,23 @@ function demo_auto_grid($add, $element, $speed) {
             }
 
             function fetchData () {
+
+                if (window.data_cache.length < 2000)
                 $.ajax({
                     dataType: "json",
                     url: <?php get_site_url() ?>"/wp-json/myplugin/v1/demo",
                     // data: data,
                     success: function(data) {
-                        window.data = data;
-                        window.index = 0;
+                        window.data_cache=window.data_cache.concat(data);
                         // console.log(data);
                     }
                 });
             }
-            window.setInterval(fetchData, <?php echo($speed* 10); ?> )
 
             function displayData() {
 
 
-                    if (!window.data) {
+                    if (window.data_cache == undefined || window.data_cache.length == 0) {
                         console.log("waiting for first data");
                         return;
                     }
@@ -174,10 +202,10 @@ function demo_auto_grid($add, $element, $speed) {
                     // items[Math.floor(Math.random()*items.length)]
                     var item = $("#"+window.coords[window.current_coord]);
 
-                    if (!item.is(":hover")) {
+                    if (!item.is(":hover") ) {
 
                         // $(item).empty();
-                        var d = window.data[window.index];
+                        var d = window.data_cache[Math.floor(Math.random()*window.data_cache.length)];
                         // $(item).attr("href", d['link']);
                         // $(item).find("img").attr('src', d['img']);
 
@@ -186,9 +214,11 @@ function demo_auto_grid($add, $element, $speed) {
                         $(img).fadeOut().queue(function() {
                             var img = $(this);
                             $(img).attr('src', d['img']);
-                            $(item).find("a").attr("href", d['link']);
+                            $(item).find("a").attr("href", d['link']).attr('alt', d['name'] );
+                            $(item).find("a").attr('title', d['name'] );
 
-                            <?php if ($add == 1) { ?>
+
+                        <?php if ($add == 1) { ?>
 
                             // console.log(window.last_update);
                             // console.log(window.idleTime);
@@ -197,7 +227,7 @@ function demo_auto_grid($add, $element, $speed) {
                             {
                                 window.last_update = new Date().getTime();
                             }
-                            else if ( new Date().getTime() - window.last_update > 10000 /*ms*/ && window.idleTime > 60 /* seconds */) {
+                            else if ( new Date().getTime() - window.last_update > window.update_limit_ms /*ms*/ && window.idleTime > window.idle_limit_sec /* seconds */) {
 
 
                                 // $('iframe').fadeOut().queue(function() {
@@ -230,7 +260,7 @@ function demo_auto_grid($add, $element, $speed) {
                         // $(item) .append($ ( "<a href='" +d['link']+ "'><img src='"+d['img']+"'/></a>") );
                     }
                     // $(item).append(("foo"));//.attr("href", window.data[window.index]['url']) );
-                    window.index = (window.index + 1) % window.data.length;
+                    // window.index = (window.index + 1) % window.data.length;
                     window.current_coord = (window.current_coord + 1) % window.coords.length;
 
             //
@@ -247,7 +277,6 @@ function demo_auto_grid($add, $element, $speed) {
                 }
             }
 
-            window.setInterval(displayData, <?php echo($speed); ?>);
 
         </script>
 
@@ -257,10 +286,11 @@ function demo_auto_grid($add, $element, $speed) {
                 border: 0;
             }
 
+
             img, .cms a>img, .jadu-cms a>img {
                 display: block;
-                max-width: 256px;
-                max-height: 256px;
+                max-width: <?php echo($px_size)?>px;
+                max-height: <?php echo($px_size)?>px;
                 margin: auto;
                 width: auto;
                 height: auto;
@@ -277,11 +307,13 @@ function demo_auto_grid($add, $element, $speed) {
             }
 
             .cell {
-                background-color:  #51738c;
-                min-width: 256px;
-                min-height: 256px;
+                background-color: white; /*  #51738c;*/
+                min-width: <?php echo($px_size)?>px;
+                min-height: <?php echo($px_size)?>px;
                 border: 0px;
             }
+
+
         </style>
         <?php
 }
@@ -290,19 +322,32 @@ add_shortcode( 'demo', 'demo_iframe' );
 
 function demo_iframe($atts) {
 
-    if ( $atts != null )
+    ob_start();
+
+    if ( $atts != null ) {
         $height = $atts['height'];
-    else
-        $height = "100%";
+        $px_size = $atts['px_size'];
+    }
+
+    if (!isset($height))
+        $height = "512px";
+
+    if (!isset($px_size))
+        $px_size = 128;
 
     ?>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <?php
 
-    demo_auto_grid(0, "'.auto-grid'", 600);
+    demo_auto_grid(0, $px_size, "'.auto-grid'", 600, -1);
+
+    ?>
+
+    <div style="border-width: 10; border-color: black">
+    <div style='width:100%; height:<?php echo($height) ?> ; overflow:hidden' class='auto-grid'> </div>
+    </div>
+<?php
 
 
-    echo ("<div style='width:100%; height:".$height."; overflow:hidden' class='auto-grid'> </div>");
-
-
+    return ob_get_clean();
 }
